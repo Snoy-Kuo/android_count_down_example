2ec419fe3f57d7f3380afdf44efcdddd105ea1ea
Snoy Kuo <snoy.kuo@twprotech.com>
2022-03-02 17:08:36 +0800

    create project.

---
+++ app/src/main/java/com/snoy/count_down_example/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.snoy.count_down_example
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.snoy.count_down_example.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
---
+++ app/src/main/java/com/snoy/count_down_example/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package com.snoy.count_down_example.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
