c6f6217e194ce467457978136701c747a7dc746a
Snoy Kuo <snoy.kuo@twprotech.com>
2022-03-04 09:49:33 +0800

    fix warnings, reformat code.

--- app/src/main/java/com/snoy/count_down_example/ui/main/MainFragment.kt
+++ app/src/main/java/com/snoy/count_down_example/ui/main/MainFragment.kt
@@ -1,11 +1,11 @@
 package com.snoy.count_down_example.ui.main
 
-import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
 import com.snoy.count_down_example.R
 
 class MainFragment : Fragment() {
@@ -23,10 +23,9 @@ class MainFragment : Fragment() {
         return inflater.inflate(R.layout.main_fragment, container, false)
     }
 
-    override fun onActivityCreated(savedInstanceState: Bundle?) {
-        super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         // TODO: Use the ViewModel
     }
-
 }
